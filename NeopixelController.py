6862ff14a1ca3ccf0d214f845a46a86248e4f5e9
Goose <63527508+GooseBehindTheSlaughter@users.noreply.github.com>
2024-01-30 02:52:40 +0000

    Create NeopixelController.py

---
+++ NeopixelController.py
@@ -0,0 +1,41 @@
+# Based on # https://github.com/blaz-r/pi_pico_neopixel/blob/main/neopixel.py
+
+import neopixel	
+import machine
+import time
+
+```
+This is currently designed to work with WS2812B leds as far as i can tell
+to get this working with other strips is to just change the colour order ("GRB") in init
+
+```
+
+class Colour:
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    YELLOW = (255, 255, 0)
+    MAGENTA = (255, 0, 255)
+    CYAN = (0, 255, 255)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0) # Same as off
+
+
+## Main Class
+class Controller:
+    def __init__(self,numLeds:int,ledPin,brightness:int=50):
+        self.numLeds= numLeds
+        self.brightness= brightness
+        
+        ## WS2812B has GRB Colour order change this according to yours
+        self.strip = neopixel.Neopixel(num_leds=self.numLeds,pin=ledPin,mode="GRB")
+        self.strip.brightness(brightness)
+        
+    def colourChase(self,color:Colour):
+        for i in range(self.numLeds):
+            self.strip[i] = color
+            time.sleep(0.1)
+            self.strip.show()
+
+    def fill(self,colour:Colour):
+        self.strip.fill(colour)
